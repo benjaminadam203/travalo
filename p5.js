eb2b7c3e1669d88b935447156def9c833f1e87aa
benjaminadam203 <benjamin.adam@escola.pr.gov.br>
2024-09-23 11:39:40 -0300

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background("#00BCD4");
+  
+}
+
+function draw() {
+  stroke("#00BCD4")
+  fill("#FFEB3B");
+ if (mouseIsPressed)
+  rect(mouseX,mouseY,20,20);
+{
+}
+}
